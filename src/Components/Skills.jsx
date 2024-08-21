adbfd326d2d47975d5ffaaf4457ba937f873c2b5
pmi0603 <prashantmishra06032003@gmail.com>
2024-08-21 23:10:36 +0530

    commit

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -6,20 +6,20 @@ function Skills() {
         <section id="skills">
             <div className="skill-container">
                 {
-<><img src="/images/c.png" alt="C" className="skill-img" />
-<img src="/images/c++.png" alt="C++" className="skill-img" />
-<img src="/images/Nodejs.png" alt="Nodejs" className="skill-img" />
+<><img src="/images/letter-c.png" alt="C" className="skill-img" />
+<img src="/images/C++.png" alt="C++" className="skill-img" />
+<img src="/images/node-js.png" alt="Nodejs" className="skill-img" />
 <img src="/images/php.png" alt="PHP" className="skill-img" /><br></br>
 <img src="/images/html.png" alt="HTML" className="skill-img" />
 <img src="/images/css.png" alt="CSS" className="skill-img" />
-<img src="/images/javascript.png" alt="JavaScript" className="skill-img" /><br></br>
-<img src="/images/react.png" alt="Reactjs" className="skill-img" />
-<img src="/images/express.png" alt="Expressjs" className="skill-img" />
-<img src="/images/mysql.png" alt="MySql" className="skill-img" />
-<img src="/images/mongodb.png" alt="MongoDB" className="skill-img" /><br></br>
+<img src="/images/js.jpeg" alt="JavaScript" className="skill-img" /><br></br>
+<img src="/images/react-js.png" alt="Reactjs" className="skill-img" />
+<img src="/images/express-js.png" alt="Expressjs" className="skill-img" />
+<img src="/images/MYSQL-Logo.jpg" alt="MySql" className="skill-img" />
+<img src="/images/mongo.png" alt="MongoDB" className="skill-img" /><br></br>
 <img src="/images/git.png" alt="GIT" className="skill-img" />
-<img src="/images/Jenkins.jpg" alt="Jenkins" className="skill-img" />
-<img src="/images/gcp.png" alt="Google Cloud Platform" className="skill-img" /><br></br> </>
+<img src="/images/jenkins.png" alt="Jenkins" className="skill-img" /><br></br>
+ </>
       }
             </div>
         </section>
