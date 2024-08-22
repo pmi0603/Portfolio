45f972644352dfea77d313d4b4d550b409c660c3
pmi0603 <prashantmishra06032003@gmail.com>
2024-08-22 14:53:57 +0530

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,8 +18,13 @@ function Home() {
 
     return (
         <div id="heading">
+         <div id="heading-container">
+          <div id="heading-content">
             <h1>Hi, {greeting}<br /> I am Prashant Mishra</h1>
             <h4>Computer Science Graduate</h4>
+          </div>
+         <img id="picture" src="/images/selfimg.jpg" alt="pic" />
+         </div>   
         </div>
     );
 }
